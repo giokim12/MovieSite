35c5bba8ea259f0ac21069c1e0fe7d31413b6555
giokim12 <giokim1011@gmail.com>
2022-11-16 13:33:38 +0900

    Feat: Popularity 기준 내림차순 정렬 페이지 연동
    컴포넌트 구조 전체적으로 수정 필요

---
+++ final-pjt-front/src/views/Main/MainView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>이거는 메인 페이지</h1>
+    <PopularMovieList/>
+  </div>
+</template>
+
+<script>
+import PopularMovieList from '@/views/Main/components/PopularMovieList'
+
+export default {
+  name: 'MainView',
+  components: {
+    PopularMovieList,
+  },
+  created() {
+    this.getPopularMovies()
+  },
+  methods: {
+    getPopularMovies() {
+      this.$store.dispatch('getPopularMovies')
+    },
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ final-pjt-front/src/views/Main/components/PopularMovieList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>유명도(?)에 따른 내림차순 정렬입니당</h2>
+    <MovieListItem
+      v-for = "movie in movies"
+      :key = "movie.id"
+      :movie = "movie"
+    />
+  </div>
+</template>
+
+<script>
+import MovieListItem from '@/views/Main/components/MovieListItem'
+
+export default {
+  name: "PopularMovieList",
+  components: {
+    MovieListItem
+  },
+  computed: {
+    movies () {
+      return this.$store.state.movies
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
