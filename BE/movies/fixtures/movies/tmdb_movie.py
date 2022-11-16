5d3d59671f9a86709bffadaf27bd50144506cad6
moonthree <fpdhslr0725@gmail.com>
2022-11-16 13:32:25 +0900

    feat: actorlist 추가

--- BE/movies/fixtures/movies/tmdb_movie.py
+++ BE/movies/fixtures/movies/tmdb_movie.py
@@ -1,18 +1,20 @@
 import requests
 import json
-
+import os
 TMDB_API_KEY = '94cfb0b050444c186251cd8dee48a17d'
+
+movie_id = []
 
 
 def get_movie_datas():
     total_data = []
     # 1페이지부터 500페이지까지 (페이지당 20개, 총 10,000개)
-    for i in range(1, 250):
+    for i in range(1, 10):
         request_url = f"https://api.themoviedb.org/3/movie/popular?api_key={TMDB_API_KEY}&language=ko-KR&page={i}"
         movies = requests.get(request_url).json()
 
         for movie in movies['results']:
-            if movie.get('release_date', '') and movie.get('adult') == False:
+            if movie.get('adult') == False and movie.get('release_date', ''):
                 fields = {
                     'movie_id': movie['id'],
                     'title': movie['title'],
@@ -29,11 +31,37 @@ def get_movie_datas():
                     "model": "movies.movie",
                     "fields": fields
                 }
-
+                movie_id.append(movie['id'])
                 total_data.append(data)
 
     with open("movie.json", "w", encoding="utf-8") as w:
         json.dump(total_data, w, indent="\t", ensure_ascii=False)
 
 
+def get_credit_datas():
+    total_data = []
+    for mid in movie_id:
+        request_url = f"https://api.themoviedb.org/3/movie/{mid}/credits?api_key={TMDB_API_KEY}&language=ko-KR"
+        get_credits = requests.get(request_url).json()
+        for credit in get_credits['cast']:
+            if credit.get('popularity') > 5 and credit.get('profile_path') != '':
+                fields = {
+                    'person_id': credit['id'],
+                    "movie_id": mid,
+                    'name': credit['name'],
+                    'popularity': credit['popularity'],
+                    'character': credit['character'],
+                    'profile_path': credit['profile_path'],
+                }
+                data = {
+                    'model': "movies.credit",
+                    "fields": fields
+                }
+                total_data.append(data)
+    print(len(total_data))
+    with open("credit.json", "w", encoding="utf-8") as w:
+        json.dump(total_data, w, indent="\t", ensure_ascii=False)
+
+
 get_movie_datas()
+get_credit_datas()
--- BE/movies/views.py
+++ BE/movies/views.py
@@ -2,8 +2,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404, get_list_or_404
-from .models import Movie, Genre
-from .serializers import MovieListSerializer
+from .models import Movie, Genre, Credit
+from .serializers import MovieListSerializer, ActorSerializer
 # Create your views here.
 
 
@@ -13,9 +13,33 @@ def movie_list(request):
         movies = get_list_or_404(Movie)
         new_movies = []
         for movie in movies:
-            if movie.vote_avg > 8:
+            if movie.vote_avg > 5:
                 new_movies.append(movie)
         # print(new_movies[0].vote_avg)
         new_movies = sorted(new_movies, key=lambda x: -x.vote_avg)
-        serializer = MovieListSerializer(new_movies, many=True)
+        new_movies10 = new_movies[:10]
+        serializer = MovieListSerializer(new_movies10, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET'])
+def movie_list_2(request):
+    if request.method == 'GET':
+        movies = get_list_or_404(Movie)
+        new_movies = []
+        for movie in movies:
+            if movie.vote_avg > 5:
+                new_movies.append(movie)
+        # print(new_movies[0].vote_avg)
+        new_movies = sorted(new_movies, key=lambda x: x.vote_avg)
+        new_movies10 = new_movies[:10]
+        serializer = MovieListSerializer(new_movies10, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET'])
+def actor_list(request):
+    if request.method == 'GET':
+        actors = get_list_or_404(Credit)
+        serializer = ActorSerializer(actors, many=True)
         return Response(serializer.data)
