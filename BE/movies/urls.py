21ad53e85d2387742c06253792a2b564c65e23b6
moonthree <fpdhslr0725@gmail.com>
2022-11-16 15:03:31 +0900

    feat: main component 복사

--- BE/movies/urls.py
+++ BE/movies/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('movies/', views.movie_list),
+    path('movies_new/', views.movie_list_new),
+    path('movies_popular/', views.movie_list_popular),
     path('actors/', views.actor_list)
 ]
