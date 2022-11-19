3b6ed49a1459f3429b5e99b54b8af4732ccde959
giokim <giokim1011@gmail.com>
2022-11-19 17:40:15 +0900

    Feat:사용자가 클릭한 영화 출력

--- final-pjt-front/src/views/Main/components/MovieListItem.vue
+++ final-pjt-front/src/views/Main/components/MovieListItem.vue
@@ -1,10 +1,13 @@
 <template>
-    <div class="m-3" @click="goDetail()">
+    <div class="m-3" @click="goDetail(), addView()">
       <img class="w-64 h-80 rounded-xl lg:hover:scale-110 transition-transform ease-in-out duration-500 hover:cursor-pointer" :src="imgPath" alt="...">
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+const API_URL = 'http://127.0.0.1:8000'
+
 export default {
   name: "MovieListItem",
   props: {
@@ -13,13 +16,34 @@ export default {
   computed: {
     imgPath() {
       return "https://image.tmdb.org/t/p/original/"+this.movie.poster_path
+    },
+    isLogin() {
+      return this.$store.state.access
     }
   },
   methods: {
     goDetail() {
       this.$router.push({ name: 'detail', params: { movie_id:this.movie.movie_id }})
+    },
+    addView() {
+      if (this.isLogin) {
+        axios ({
+          method: 'POST',
+          url: `${API_URL}/api/v1/detail/${this.movie.movie_id}/`,
+          data: {
+            user: this.$store.state.userdata.id,
+            movie: this.movie.movie_id
+          }
+        })
+          .then((res) => {
+            console.log(res)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      }
     }
-  }
+  },
 }
 </script>
 
