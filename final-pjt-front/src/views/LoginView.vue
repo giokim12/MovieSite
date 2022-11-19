9bba2a67f29b4a73a9b1375609827675d90fc146
giokim <giokim1011@gmail.com>
2022-11-19 14:21:39 +0900

    Style: 회원가입 페이지, 파비콘 등 수정
    알고리즘 시작해야지...

--- final-pjt-front/src/views/LoginView.vue
+++ final-pjt-front/src/views/LoginView.vue
@@ -13,7 +13,7 @@
               </h1>
               <form v-on:submit.prevent="submitForm" class="space-y-4 md:space-y-6" action="#">
                   <div>
-                      <label for="username" class="block mb-2 text-sm font-medium text-gray-900 text-white">아이디</label>
+                      <label for="username" class="block mb-2 text-sm font-medium text-white">아이디</label>
                       <input v-model="username" type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5" required="">
                   </div>
                   <div>
@@ -22,7 +22,7 @@
                   </div>
                   <button type="submit" class="w-full text-white bg-red-400 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-red-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center">로그인</button>
                   <p class="text-sm font-light text-gray-500 text-white">
-                      JWT 회원이 아닌가요? <a href="#" class="font-medium text-red-400 hover:underline text-white">지금 가입하세요.</a>
+                      JWT 회원이 아닌가요? <a href="#" class="font-medium text-red-400 hover:underline text-red-300">지금 가입하세요.</a>
                   </p>
               </form>
           </div>
