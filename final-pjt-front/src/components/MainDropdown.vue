4793c59e726bb2770a26258a629cd1b51bcaa617
moonthree <fpdhslr0725@gmail.com>
2022-11-24 07:59:28 +0900

    feat:nav 마이페이지 추가

---
+++ final-pjt-front/src/components/MainDropdown.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-center">
+    <div class="relative">
+      <div v-if="navdrop" @click="navdrop = false" class="fixed inset-0"></div>
+      <slot :open="navdrop" :toggleOpen="toggleOpen"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navdrop: false
+    };
+  },
+  methods: {
+    toggleOpen() {
+      this.navdrop = !this.navdrop;
+    }
+  }
+};
+</script>
